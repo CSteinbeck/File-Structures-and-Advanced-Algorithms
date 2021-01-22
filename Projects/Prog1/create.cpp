88226d37075e38f55773762db20ee0725c19cd60
Colton R. Steinbeck <steinbeck_cr@cobra.cs.mercer.edu>
2021-01-21 19:14:51 -0500

    Creation of the create.cpp file

---
+++ Projects/Prog1/create.cpp
@@ -0,0 +1,22 @@
+//Colton Steinbeck
+//Part II: Processing a Master File and Building a Binary File
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+typedef char String[25];
+struct BookRec
+{
+    unsigned int isbn;
+    String name;
+    String author;
+    int onhand;
+    float price;
+    String type;
+};
+
+int main(int argc, char* argv[])
+{
+    
+}
