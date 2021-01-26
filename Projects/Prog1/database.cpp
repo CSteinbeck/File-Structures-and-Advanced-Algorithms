83c729857ad81a47595f2c683639a5d875f1d09a
Colton Steinbeck <colton@steinbeck-home.com>
2021-01-26 19:02:17 +0000

    Creation of Part III : Database

---
+++ Projects/Prog1/database.cpp
@@ -0,0 +1,36 @@
+//Colton Steinbeck
+//Creation of the Database for Binary Books
+
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <map>
+#include <iomanip>
+#include <climits>
+
+using namespace std;
+
+//Needed emunerators and structs
+typedef char String[25];
+struct BookRec
+{
+    unsigned int isbn;
+    String name;
+    String author;
+    int onhand;
+    float price;
+    String type;
+};
+
+enum TransactionType {Add, Delete, ChangeOnHand, ChangePrice};
+struct TransactionRec
+{
+    TransactionType ToDo;
+    BookRec B;
+};
+
+
+int main(int argc, char* argv[])
+{
+    return 0;
+}
