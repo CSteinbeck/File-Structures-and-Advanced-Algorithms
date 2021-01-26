c22d7ab77304e0786f84439b11383638cd78cd1a
Colton R. Steinbeck <steinbeck_cr@cobra.cs.mercer.edu>
2021-01-25 22:27:12 -0500

    Update Create.cpp

--- Projects/Prog1/create.cpp
+++ Projects/Prog1/create.cpp
@@ -8,6 +8,7 @@
 
 using namespace std;
 
+//Given Struct of Book Record
 typedef char String[25];
 struct BookRec
 {
@@ -34,26 +35,25 @@ int main(int argc, char* argv[])
 {
     //Creates input file argument and reads it in
     BookRec BookInstance;
-    string filename =argv[1];
+    string filename =argv[1]; //Filename is second parameter
     ifstream readInput (filename.c_str(), ios :: in | ios :: binary);
     string delta;
-    long long tempIsbn; //
+    long long tempIsbn;
     long savedIsbn;
     fstream output("library.out",ios :: out | ios :: binary);
     //Reading readInput into temp isbn
     while(readInput>>tempIsbn)
     {
         bool out = true; //The output can occur and go into the file, can stop records from being printed to the binary file
-        //Ignore the first delimiter for the byte
-        readInput.ignore(1,'|');
+        readInput.ignore(1,'|'); //Ignore the first delimiter for the byte
         if(tempIsbn < 1)
         {
-            cerr<<"YOU MESSED UP MY BOY"<<endl;
+            cerr<<"Error: ISBN is invalid"<<endl;
             getline(readInput, delta); //Moves the needle from the error to the next isbn     
         }
         else
         {   
-            //
+            //Defines items in the .dat file in context to the struct
             BookInstance.isbn = tempIsbn;
             readInput.getline(BookInstance.name,25, '|');
             readInput.getline(BookInstance.author,25, '|');
@@ -66,7 +66,7 @@ int main(int argc, char* argv[])
 
             if(tempIsbn < savedIsbn)
             {
-                cerr<<"This isbn is out of order"<<endl;
+                cerr<<"Isbn is out of order"<<endl;
             }
             if(BookInstance.price < 0)
             {
@@ -75,10 +75,11 @@ int main(int argc, char* argv[])
             }
             if(BookInstance.onhand <0)
             {
-                cerr<<"There are no books in store!"<<endl;
+                cerr<<"There are no more copies on hand!"<<endl;
                 out = false;
             }
             savedIsbn = BookInstance.isbn;
+            
             //Type-casting into a char * and then being written into a binary file
             if(out)
             {
@@ -90,6 +91,8 @@ int main(int argc, char* argv[])
     readInput.close();
     output.close();
 
+
+    //Printing out items in correspondance to library.out file
     ifstream output2("library.out", ios :: in | ios :: binary);
     //Prints values to the screen
     while(output2.read((char *) &BookInstance,sizeof(BookInstance)))
