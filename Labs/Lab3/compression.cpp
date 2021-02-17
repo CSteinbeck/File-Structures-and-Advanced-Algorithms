df6f1d22fdcde781c97416dc87990e1d6126d4b4
Colton R. Steinbeck <steinbeck_cr@cobra.cs.mercer.edu>
2021-02-16 23:03:58 -0500

    Compression Algorithm: Semi-Working Version

--- Labs/Lab3/compression.cpp
+++ Labs/Lab3/compression.cpp
@@ -2,39 +2,62 @@
 #include <cstdlib>
 #include <fstream>
 #include <sstream>
+#include <string>
+#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-string compress(stringstream &s1)
-{
-    string test1 = "Compression works";
-    return test1;  
-}
+string compress(string &myInput)
+{   
+    int difference;
+    vector<int> compressionVector;
+    string result= " "; int num; 
+    stringstream s1(myInput);
+    stringstream outputStream;
+    while(s1>>num)
+    {
+         compressionVector.push_back(num);
 
-string uncompress(stringstream &s1)
-{
-    string test2 = "Uncompression works";
-    return test2; 
-}
+            int freq = count(compressionVector.begin(),compressionVector.end(),num); //Identifies the frequency as the number of times that number appears
+            if(freq < 4)
+            { 
+                outputStream<<num;
+                outputStream<<" ";
+            }
+            else
+            {
+              outputStream<< "ff ";
+              outputStream <<num << " ";
+              outputStream<<freq<< " "; 
+              do{
+                s1>>num;
+                freq--;
+              }while(freq>1); 
+            }
+
+         }
+
+        result= outputStream.str();
+        return result;
+    }
+    
+
+
+// string uncompress(string &cResult)
+// {
+//     string result;
+//     return result;
+// }
 
 
 int main(int argc, char *argv[])
 {
     string result; //Final result character
-    string userInput; //User input variable
-    string number; //String to pass items into the string stream
-    cout<<"Please enter the string: "<<endl;
-    getline(cin, userInput); //Gets the user input
-    stringstream s1(userInput); //Imports it into the stringstream
-    while(s1>>number) //While the stream is being passed into the string number object
-    {
-        //cout<<number<<endl; --Test case for the stringstream
-        if(number == "ff") //Checks if the stringstream is a compressed file
-        {
-            result = uncompress(s1); //Calls the Uncompression Function
-        }
-       result = compress(s1);
-    }
-    cout<<result<<endl;
+    string myInput = "93 93 93 93 92 91 91 94 94 94 94 94 95 95 95 73 73 73 73 73 73 73";
+    string cResult = compress(myInput);
+    cout<<cResult<<endl;
+    // string uResult =uncompress(cResult);
+    // cout<<uResult<<endl;
     return 0;
 }
