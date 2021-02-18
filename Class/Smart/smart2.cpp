05a1a2c4b5e30795faa25ac44c5a725c91ffac4e
Colton R. Steinbeck <steinbeck_cr@cobra.cs.mercer.edu>
2021-02-18 16:33:04 -0500

    Smart Pointer Introduction

--- Class/Smart/smart2.cpp
+++ Class/Smart/smart2.cpp
@@ -6,14 +6,14 @@ using namespace std;
 
 int main()
 {
-	unique_ptr<int> valuePtr(new int(15));
-        valuePtr.reset(new int(25));
-	unique_ptr<int> valuePtrNow(move(valuePtr));
+	unique_ptr<int> valuePtr(new int(15)); //Shared Pointer
+        valuePtr.reset(new int(25)); //.reset changes the pointer value
+	unique_ptr<int> valuePtrNow(move(valuePtr)); //
 
         cout << "valuePtrNow = " << *valuePtrNow << endl;
         cout << "Has valuePtr an associated object? "
              << boolalpha << " "
-             << (bool)(valuePtr) << endl;
+             << (bool)(valuePtr) << endl; //Ddes a true or false on a ptr
         cout << "Has valuePtrNow an associated object? "
              << (bool)(valuePtrNow) << endl;
 
--- Class/Smart/smart3.cpp
+++ Class/Smart/smart3.cpp
@@ -6,7 +6,7 @@ using namespace std;
 class Entity
 {
 	public:
-		Entity() {  cout << "Created Entity!" << endl; }
+		Entity() {  cout << "Created Entity!" << endl; } //Used to show every time the constructor/deconstructor is called
 		~Entity() {  cout << "Destroyed Entity!" << endl; }
                 void Print() { cout << "Mardi Gras!" << endl; }
 };
@@ -15,10 +15,10 @@ int main()
 {
 
 	unique_ptr<Entity> entity(new Entity());
-        entity -> Print();
+        entity -> Print(); //-> is a reference operator and printed
 
         shared_ptr<Entity> sharedEntity = make_shared<Entity>();
-        shared_ptr<Entity> e0 = sharedEntity;
+        shared_ptr<Entity> e0 = sharedEntity; //e0 is an alias
 
 	return 0;
 }
