7f4688252ddaac2f11b279e0be34d395b06f45ca
Colton R. Steinbeck <steinbeck_cr@cobra.cs.mercer.edu>
2021-02-18 20:25:47 -0500

    Completed Compression and Uncompression

--- Labs/Lab3/compression.cpp
+++ Labs/Lab3/compression.cpp
@@ -16,32 +16,34 @@ string compress(string &myInput)
     stringstream s1(myInput);
     stringstream outputStream;
     s1>>prev;
-    compressionVector.push_back(prev);
-    while(s1>>num)
+    compressionVector.push_back(prev); //Pushes the value in the vector
+    while(s1>>num) //While you're reading in the number
     {
-        if(num ==prev)
+        if(num ==prev) //If the previous numvber is a duplicate
         {
-          compressionVector.push_back(num);
+          compressionVector.push_back(num); //Adds the current number to the array
         }
         else
         {
           int freq = count(compressionVector.begin(),compressionVector.end(),prev); //Identifies the frequency as the number of times that number appears
-          if(freq >= 4)
+          if(freq >= 4) //If the frequency bot 
           {
+            //Inputs the compression data
             outputStream<< "ff ";
             outputStream<< prev << " ";
             outputStream<< freq<< " ";
             compressionVector.clear();
           }
           else{
+            //If the frequency is less than 4
             for(int i =0; i<=freq; i++){
-              outputStream<< prev<< " ";
+              outputStream<< prev<< " "; 
               
             }
-            compressionVector.clear();
+            compressionVector.clear(); //Clears the vector to test it again
           }
         }
-        prev = num;   
+        prev = num; //Sets the num 
     }
     int freq = count(compressionVector.begin(),compressionVector.end(),prev); //Identifies the frequency as the number of times that number appears
           if(freq >= 4)
@@ -52,9 +54,10 @@ string compress(string &myInput)
             compressionVector.clear();
           }
           else{
+          
             for(int i =0; i<=freq; i++)
             {
-              outputStream<< prev<< " ";
+              outputStream<< prev<< " "; //Prints the frequency of the number that is less than 4
               
             }
             compressionVector.clear();
@@ -66,22 +69,34 @@ string compress(string &myInput)
     
 
 
-// string uncompress(string &cResult)
-// {
-//   string curr; string prev;
-//   stringstream s2(cResult);
-//   s2>>prev;
-//   while(s2>>curr)
-//   {
-//     if(curr =="ff")
-//     {
+string uncompress(string &cResult)
+{
+  string curr;
+  int count;
+  stringstream s2(cResult);
+  stringstream outputStream2;
+  
+ 
+  while(s2>>curr)
+  {
 
-//     }
-//   }
+    if(curr == "ff")
+    {
+      s2>>curr; //curr moves the stringstream to the string we want to print out
+      s2>>count;
+      for(int i=0; i<count;i++)
+      {
+        outputStream2<<curr<<" ";
+      }
+    }
+    else{
+      outputStream2<<curr<<" ";
+    }
 
-//     string result;
-//     return result;
-// }
+  }
+  string result = outputStream2.str();
+  return result;
+}
 
 
 int main(int argc, char *argv[])
@@ -90,7 +105,7 @@ int main(int argc, char *argv[])
     string myInput = "93 93 93 93 92 91 91 94 94 94 94 94 95 95 95 73 73 73 73 73 73 73";
     string cResult = compress(myInput);
     cout<<cResult<<endl;
-    // string uResult =uncompress(cResult);
-    // cout<<uResult<<endl;
+    string uResult =uncompress(cResult);
+    cout<<uResult<<endl;
     return 0;
 }
