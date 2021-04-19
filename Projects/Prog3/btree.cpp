d3a202c943b547496eccb3aed3c0f7d7f46c0cc7
Colton R. Steinbeck <steinbeck_cr@cobra.cs.mercer.edu>
2021-04-19 00:57:59 -0400

    Creation of the BTree class and the createBT.cpp file

---
+++ Projects/Prog3/btree.cpp
@@ -0,0 +1,27 @@
+#include "album.h"
+#include "btree.h"
+
+using namespace std;
+BTree :: BTree()
+{
+    strcpy(height,"\0");
+    strcpy
+
+}
+void BTree :: 
+int Btree :: getHeight()
+{
+    return height;
+}
+bool Btree :: search(string key)
+{
+
+}
+keyType BTree :: retrieve(string key)
+{
+
+}
+int BTree :: countLeaves()
+{
+
+}
---
+++ Projects/Prog3/createBT.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdlib>
+#include <sstream>
+#include "btree.h"
+#include "album.h"
+
+
+
+using namespace std;
+
+
+
+
+
+
+
+
+
+
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
